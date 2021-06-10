398198300945a3cee3622242290f96fcf23df391
marvinjoel <marvinj.isd@gmail.com>
2021-06-10 16:55:10 -0500

    Primera funcion

---
+++ js/funcion.js
@@ -0,0 +1,13 @@
+
+// Declaramos una funcion SALUDO 
+
+function saludo(){
+    alert("Hello World: ");
+}
+
+
+// Podemos llamarlo de esta manera
+// Pero lo llamaremos desde un boton
+
+
+//saludo();
