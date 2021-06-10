810bb43aab8a53549aa208787722aee2d2fb2ef8
marvinjoel <marvinj.isd@gmail.com>
2021-06-10 15:30:46 -0500

    Condiciones

---
+++ js/condiciones.js
@@ -0,0 +1,23 @@
+// Estructuras condicionales if, else if, else
+
+// var semaforo = "negro";
+
+// if (semaforo === "verde")
+//     alert("El semaforo está en verde");
+// else if (semaforo === "rojo")
+//     alert("El semaforo está en alto");
+// else if (semaforo === "amarillo")
+//     alert("El semaforo está en Amarillo");
+// else
+//     alert("El semaforo está en: "+ semaforo);
+
+
+/*  PROMPT:  permite poner datos al usuario en modo dinamico */
+/*  NUMBER:  combierte numeros string en enteros tipo INT */
+
+var edad = Number(prompt("Ingrese su edad: "));
+
+if(edad >= 18)
+    alert("Es Mayor de edad");
+else
+    alert("Es Menor de edad");
