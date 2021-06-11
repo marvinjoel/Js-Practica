489608bb83f7c320531a32a155fcb0f3987ffa32
marvinjoel <marvinj.isd@gmail.com>
2021-06-11 18:50:33 -0500

    Funciones con parámetros

---
+++ js/funcion1.js
@@ -0,0 +1,17 @@
+
+/* 
+Crearemos una funcion SALUDO que por parametro le pondremos una variable
+*/
+
+function saludo(nombre){
+    // alert("Hola "+nombre)
+    // lo  imprimimos con DOCUMENT WRITE para mostrar por pantall
+    document.write("Hola "+nombre+"</br>")
+};
+
+// Estamos usando la funcion con parametros diferentes
+saludo("Joel");
+saludo("Maria");
+saludo("Alonso");
+saludo("Gilberto");
+saludo("Gabriela");
